06544f727aa3c1f61e8d9750421d397f90ba7b8c
HelgeLilleorg <helge.lilleorg@gmail.com>
2021-11-21 13:18:09 +0200

    no duplicates

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,20 +1,15 @@
 <template>
   <div class="mainPage">
-    <section class="content" style="padding-top: 40px">
-      <div class="resetClass">
-        <button v-on:click="resetLikes">Reset Likes</button>
-      </div>
-      <Post> </Post>
-    </section>
+   
   </div>
 </template>
 
 <script>
-import Post from "@/components/Post.vue";
+//import Post from "@/components/Post.vue";
 
 export default {
   name: "MainPage",
-  components: { Post },
+  components: {  },
   data() {
     return {};
   },
