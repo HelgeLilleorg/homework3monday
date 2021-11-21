4a7825e1737802258e7bccac6ba7cdfaf7ad9e58
HelgeLilleorg <helge.lilleorg@gmail.com>
2021-11-21 23:48:40 +0200

    style changes

--- src/App.vue
+++ src/App.vue
@@ -37,5 +37,34 @@ export default {
 
 
 }
-
+.content {
+  max-width: 500px;
+  margin: 0px;
+  margin-top: 27px;
+  background: pink;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: auto;
+  line-height: 150%;
+  text-align: center;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 20px 10px darkgrey;
+}
+.resetClass {
+  padding-bottom: 25px;
+}
+button {
+  background-color: #555;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  border-radius: 8px;
+  box-shadow: 3px 3px 10px -4px #242424;
+  cursor: pointer;
+}
 </style>
